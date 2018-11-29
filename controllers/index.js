a557a68027e8843d163a6163743197ca26460273
Matthew Li <matthewli2491@gmail.com>
2018-11-29 14:24:54 -0500

    merged and fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 // const express = require('express');
 // const router = express.Router();
 
-router.use('/read', require('./read'));
-router.use('/create', require('./create'));
-router.use('/delete', require('./delete'));
+// router.use('/read', require('./read'));
+// router.use('/create', require('./create'));
+// router.use('/delete', require('./delete'));
 
 // module.exports = router;
